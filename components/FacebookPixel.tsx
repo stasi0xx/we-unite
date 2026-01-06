f128b685ba53726695307a405492b1533a415203
stasi0xx <stanislaw.korycki.w@gmail.com>
2026-01-06 14:44:50 +0100

    Pixel Meta

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -2,32 +2,42 @@
 
 import { usePathname, useSearchParams } from 'next/navigation';
 import Script from 'next/script';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 
-export const FacebookPixel = () => {
+// 1. Wydzielamy logikę używającą hooków do osobnego komponentu
+const PixelEvents = () => {
     const [loaded, setLoaded] = useState(false);
     const pathname = usePathname();
-    const searchParams = useSearchParams();
+    const searchParams = useSearchParams(); // <--- To jest winowajca, który wymaga Suspense
 
     useEffect(() => {
         if (!loaded) return;
-
         // Track PageView on route change
         window.fbq('track', 'PageView');
     }, [pathname, searchParams, loaded]);
 
     return (
+        <Script
+            id="fb-pixel"
+            src="https://connect.facebook.net/en_US/fbevents.js"
+            strategy="afterInteractive"
+            onLoad={() => {
+                setLoaded(true);
+                window.fbq('init', '3412865108961069');
+                window.fbq('track', 'PageView');
+            }}
+        />
+    );
+};
+
+// 2. Główny komponent eksportowany na zewnątrz
+// Owija logikę w Suspense, co naprawia błąd budowania strony 404
+export const FacebookPixel = () => {
+    return (
         <>
-            <Script
-                id="fb-pixel"
-                src="https://connect.facebook.net/en_US/fbevents.js"
-                strategy="afterInteractive"
-                onLoad={() => {
-                    setLoaded(true);
-                    window.fbq('init', '3412865108961069');
-                    window.fbq('track', 'PageView');
-                }}
-            />
+            <Suspense fallback={null}>
+                <PixelEvents />
+            </Suspense>
             <noscript>
                 <img
                     height="1"
